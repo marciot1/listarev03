ea89388e21421163024977336b7371d945cef0c8
MARCIO TARGINO <105668116+marciot1@users.noreply.github.com>
2022-07-08 18:10:25 +0000

    Initial commit

---
+++ aula_0807/Main.cs
@@ -0,0 +1,14 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    int x = 10;
+    while (x >= 1){
+      Console.WriteLine(x);
+      x--;
+    }
+    
+    
+  }
+}
